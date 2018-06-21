8b242e8c3afe49c9a3d2728773c1152e271b4faa
Justin Lane <justin@justinlane.org>
2018-06-21 12:44:02 +1200

    Update Test and plugin

--- __tests__/add.js
+++ __tests__/add.js
@@ -5,13 +5,133 @@ test('adds the proper npm module and component example', async () => {
   // spy on few things so we know they're called
   const addModule = sinon.spy()
   const addPluginComponentExample = sinon.spy()
+  const patchInFile = sinon.spy()
+  const copy = sinon.spy()
+  const read = sinon.spy()
+  const name = 'ignite-react-native-navigation'
 
   // mock a context
   const context = {
-    ignite: { addModule, addPluginComponentExample }
+    ignite: { addModule, addPluginComponentExample, patchInFile },
+    filesystem: { copy, read }
   }
 
   await plugin.add(context)
 
-  expect(addModule.calledWith('react-native-navigation', {version: '2.0.2362', link: true})).toBe()
+  expect(addModule.calledWith('react-native-navigation', {version: '2.0.2362', link: true})).toEqual(true)
+  expect(patchInFile.calledWith(`${process.cwd()}/android/build.gradle`, {
+    before: `mavenLocal()`,
+    insert: `        google()
+        mavenCentral()`
+  })).toEqual(true)
+
+  expect(patchInFile.calledWith(`${process.cwd()}/android/build.gradle`, {
+    after: `repositories {`,
+    insert: `        google()
+        mavenLocal()
+        mavenCentral()`
+  })).toEqual(true)
+
+  // One line, plenty of grief ;)
+  expect(patchInFile.calledWith(`${process.cwd()}/android/build.gradle`, {
+    replace: `classpath 'com.android.tools.build:gradle:2.2.3'`,
+    insert: `classpath 'com.android.tools.build:gradle:3.0.1'`
+  })).toEqual(true)
+
+  // app/build.gradle
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/build.gradle`, {
+    replace: `compileSdkVersion 23`,
+    insert: `compileSdkVersion 25`
+  })).toEqual(true)
+
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/build.gradle`, {
+    replace: `buildToolsVersion "23.0.1"`,
+    insert: `buildToolsVersion "27.0.3"`
+  })).toEqual(true)
+
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/build.gradle`, {
+    replace: `minSdkVersion 16`,
+    insert: `minSdkVersion 19`
+  })).toEqual(true)
+
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/build.gradle`, {
+    replace: `targetSdkVersion 22`,
+    insert: `targetSdkVersion 25`
+  })).toEqual(true)
+
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/build.gradle`, {
+    replace: `targetSdkVersion 25`,
+    insert: `missingDimensionStrategy "RNN.reactNativeVersion", "reactNative55"`
+  })).toEqual(true)
+
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/build.gradle`, {
+    before: `buildTypes {`,
+    insert: `    compileOptions {
+      sourceCompatibility JavaVersion.VERSION_1_8
+      targetCompatibility JavaVersion.VERSION_1_8
+    }`
+  })).toEqual(true)
+
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/build.gradle`, {
+    replace: `compile "com.android.support:appcompat-v7:23.0.1"`,
+    insert: `implementation "com.android.support:appcompat-v7:25.4.0"`
+  })).toEqual(true)
+
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/build.gradle`, {
+    replace: `compile fileTree(dir: "libs", include: ["*.jar"])`,
+    insert: `implementation fileTree(dir: "libs", include: ["*.jar"])`
+  })).toEqual(true)
+
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/build.gradle`, {
+    after: `implementation "com.android.support:appcompat-v7:25.4.0"`,
+    insert: `    implementation project(':react-native-navigation')`
+  })).toEqual(true)
+
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/build.gradle`, {
+    replace: `    compile "com.`,
+    insert: `    implementation "com.`,
+    force: true
+  })).toEqual(true)
+
+  // android/gradle/wrapper/gradle-wrapper.properties
+  expect(patchInFile.calledWith(`${process.cwd()}/android/gradle/wrapper/gradle-wrapper.properties`, {
+    replace: 'gradle-2.14.1-all.zip',
+    insert: 'gradle-4.4-all.zip',
+    force: true
+  })).toEqual(true)
+
+  // android/gradle.properties
+  expect(patchInFile.calledWith(`${process.cwd()}/android/gradle.properties`, {
+    after: `android.useDeprecatedNdk=true`,
+    insert: `android.enableAapt2=false`
+  })).toEqual(true)
+
+  // MainActivity.java
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/src/main/java/com/${name.toLowerCase()}/MainActivity.java`, {
+    replace: 'import com.facebook.react.ReactActivity;',
+    insert: 'import com.reactnativenavigation.NavigationActivity;'
+  })).toEqual(true)
+
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/src/main/java/com/${name.toLowerCase()}/MainActivity.java`, {
+    replace: 'public class MainActivity extends ReactActivity {',
+    insert: 'public class MainActivity extends NavigationActivity {'
+  })).toEqual(true)
+
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/src/main/java/com/${name.toLowerCase()}/MainActivity.java`, {
+    delete: '@Override'
+  })).toEqual(true)
+
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/src/main/java/com/${name.toLowerCase()}/MainActivity.java`, {
+    delete: '    protected String getMainComponentName() {'
+  })).toEqual(true)
+
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/src/main/java/com/${name.toLowerCase()}/MainActivity.java`, {
+    delete: `return "${name.toLowerCase()}";`
+  })).toEqual(true)
+
+  expect(patchInFile.calledWith(`${process.cwd()}/android/app/src/main/java/com/${name.toLowerCase()}/MainActivity.java`, {
+    delete: `    }`
+  })).toEqual(true)
+
+  expect(patchInFile.callCount).toEqual(21)
 })
--- plugin.js
+++ plugin.js
@@ -3,15 +3,18 @@
 
 const NPM_MODULE_NAME = 'react-native-navigation'
 const NPM_MODULE_VERSION = '2.0.2362'
+const jetpack = require('fs-jetpack')
+const NPM_PACKAGE = jetpack.read('package.json', 'json')
+// :point_up: This is kind of dirty from a test perspective as we're relying on
+// the tests to be run in the root directory. Better Implementations welcome!
 
 // const PLUGIN_PATH = __dirname
 // const APP_PATH = process.cwd()
 
 const add = async function (context) {
   // Learn more about context: https://infinitered.github.io/gluegun/#/context-api.md
-  const { ignite, filesystem } = context
-  const NPMPackage = await filesystem.read('package.json', 'json')
-  const name = NPMPackage.name
+  const { ignite } = context
+  const name = NPM_PACKAGE.name
 
   // Android Install
 
@@ -44,22 +47,22 @@ const add = async function (context) {
   ignite.patchInFile(`${process.cwd()}/android/app/build.gradle`, {
     replace: `buildToolsVersion "23.0.1"`,
     insert: `buildToolsVersion "27.0.3"`
-  })  
+  })
 
   ignite.patchInFile(`${process.cwd()}/android/app/build.gradle`, {
     replace: `minSdkVersion 16`,
-    insert: `minSdkVersion 19`,
+    insert: `minSdkVersion 19`
   })
 
   ignite.patchInFile(`${process.cwd()}/android/app/build.gradle`, {
     replace: `targetSdkVersion 22`,
     insert: `targetSdkVersion 25`
-  })  
+  })
 
   ignite.patchInFile(`${process.cwd()}/android/app/build.gradle`, {
     replace: `targetSdkVersion 25`,
     insert: `missingDimensionStrategy "RNN.reactNativeVersion", "reactNative55"`
-  })  
+  })
 
   ignite.patchInFile(`${process.cwd()}/android/app/build.gradle`, {
     before: `buildTypes {`,
@@ -67,28 +70,28 @@ const add = async function (context) {
       sourceCompatibility JavaVersion.VERSION_1_8
       targetCompatibility JavaVersion.VERSION_1_8
     }`
-  })  
+  })
 
   ignite.patchInFile(`${process.cwd()}/android/app/build.gradle`, {
     replace: `compile "com.android.support:appcompat-v7:23.0.1"`,
     insert: `implementation "com.android.support:appcompat-v7:25.4.0"`
-  })  
+  })
 
   ignite.patchInFile(`${process.cwd()}/android/app/build.gradle`, {
     replace: `compile fileTree(dir: "libs", include: ["*.jar"])`,
     insert: `implementation fileTree(dir: "libs", include: ["*.jar"])`
-  })  
+  })
 
   ignite.patchInFile(`${process.cwd()}/android/app/build.gradle`, {
     after: `implementation "com.android.support:appcompat-v7:25.4.0"`,
     insert: `    implementation project(':react-native-navigation')`
-  })  
+  })
 
   ignite.patchInFile(`${process.cwd()}/android/app/build.gradle`, {
     replace: `    compile "com.`,
     insert: `    implementation "com.`,
     force: true
-  })  
+  })
 
   // android/gradle/wrapper/gradle-wrapper.properties
   // 2.2 versions is not really much of a jump :scream:
@@ -96,27 +99,27 @@ const add = async function (context) {
     replace: 'gradle-2.14.1-all.zip',
     insert: 'gradle-4.4-all.zip',
     force: true
-  })  
+  })
 
   // android/gradle.properties
   ignite.patchInFile(`${process.cwd()}/android/gradle.properties`, {
     after: `android.useDeprecatedNdk=true`,
     insert: `android.enableAapt2=false`
-  })  
+  })
 
   // MainActivity.java
   ignite.patchInFile(`${process.cwd()}/android/app/src/main/java/com/${name.toLowerCase()}/MainActivity.java`, {
     replace: 'import com.facebook.react.ReactActivity;',
     insert: 'import com.reactnativenavigation.NavigationActivity;'
   })
-  
+
   ignite.patchInFile(`${process.cwd()}/android/app/src/main/java/com/${name.toLowerCase()}/MainActivity.java`, {
     replace: 'public class MainActivity extends ReactActivity {',
     insert: 'public class MainActivity extends NavigationActivity {'
   })
-  
+
   ignite.patchInFile(`${process.cwd()}/android/app/src/main/java/com/${name.toLowerCase()}/MainActivity.java`, {
-    delete: '@Override',
+    delete: '@Override'
   })
 
   ignite.patchInFile(`${process.cwd()}/android/app/src/main/java/com/${name.toLowerCase()}/MainActivity.java`, {
@@ -133,7 +136,7 @@ const add = async function (context) {
 
   // install an NPM module and link it
   // Note to self. rn link writes to the pbxproj.
-  // await ignite.addModule(NPM_MODULE_NAME, { link: true, version: NPM_MODULE_VERSION })
+  await ignite.addModule(NPM_MODULE_NAME, { link: true, version: NPM_MODULE_VERSION })
 
   // Example of copying templates/ReactNativeNavigation to App/ReactNativeNavigation
   // if (!filesystem.exists(`${APP_PATH}/App/ReactNativeNavigation`)) {
